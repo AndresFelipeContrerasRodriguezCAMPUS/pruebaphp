8bbdf30d1b951d527ef76fe9a1a04b9556bc0eb7
AndresFelipeContrerasRodriguezCAMPUS <felipecr3456@gmail.com>
2023-06-28 09:39:28 -0500

    nuevo

--- conection/config.php
+++ conection/config.php
@@ -2,10 +2,49 @@
 
 
 return function(Connection &$connection){
-$connection.add(
-    "campuslands",
-    ["mysql:localhost;","dbname=campuslands","campus","campus2023"]
-);
+    $settings = Array(
+        'db' => Array(
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'username' => 'campus',
+            'database' => 'campuslands',
+            'password' => 'campus2023',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'flags' => [
+                // Turn off persistent connections
+                PDO::ATTR_PERSISTENT => false,
+                // Enable exceptions
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                // Emulate prepared statements
+                PDO::ATTR_EMULATE_PREPARES => true,
+                // Set default fetch mode to array
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                // Set character set
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+            ]
+        ),
+        'db2' => Array(
+            'driver' => 'pgsql',
+            'host' => 'localhost',
+            'username' => 'madocoxxi',
+            'database' => 'madoco',
+            'password' => 'madoco21',
+            'flags' => [
+                // Turn off persistent connections
+                PDO::ATTR_PERSISTENT => false,
+                // Enable exceptions
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                // Set default fetch mode to array
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                // Set character set
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+            ]
+        )
+    );
+
+    return $settings;
 }
+?>
 
 ?>
